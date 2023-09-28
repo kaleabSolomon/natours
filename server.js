7c3a3148d755d081b1acfab52589a8d5b55339ca
Kaleab Solomon <kaleabslmn@gmail.com>
2023-09-28 17:36:49 +0300

    create tour model

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ const tourSchema = new mongoose.Schema({
   }
 });
 
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
