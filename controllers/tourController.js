b259f643ca12af6390595f925501e8972bc8af7a
Kaleab Solomon <kaleabslmn@gmail.com>
2023-10-03 20:10:33 +0300

    add pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,6 +39,19 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+    //pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('Page does not exist');
+      }
+    }
     //executing the query
     const tours = await query;
 
