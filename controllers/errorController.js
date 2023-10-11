43e17213ab56060f718a821b7c1acacc5ce8c188
Kaleab Solomon <kaleabslmn@gmail.com>
2023-10-11 20:56:01 +0300

    handling invalid db ids

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDb = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -28,6 +35,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.Node_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.Node_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') {
+      error = handleCastErrorDb(error);
+    }
+    sendErrorProd(error, res);
   }
 };
