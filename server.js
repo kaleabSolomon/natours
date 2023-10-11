6b4263d54b450a30cf9c98d9120404e75aab05d8
Kaleab Solomon <kaleabslmn@gmail.com>
2023-10-12 00:08:26 +0300

    unhandled rejections

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ mongoose
   .then(() => {
     console.log('DB connection established');
   });
-// creating schema
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('unhandled rejection');
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
 });
