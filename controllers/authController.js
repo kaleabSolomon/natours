00ae329f99187cdcefd7d099b5f132eeccf49a0e
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-05 19:58:00 +0300

    add roles to users

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt
+    role: req.body.role
+    // passwordChangedAt: req.body.passwordChangedAt
   });
 
   const token = signToken(newUser._id);
