8ca0972ba9dc0e7df122b64711a77f9990e851b9
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-07 15:46:04 +0300

    get user based on reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -127,4 +128,18 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     return next(new AppError('there was an error sending the email', 500));
   }
 });
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // get user based on token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+  // if token is not expired and there is a user, set new password
+  // update changedPasswordAt property
+  // log the user in
+});
