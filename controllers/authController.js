4cd1802ccabef7c11e553cf0d1c0ae75c23c1367
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-07 21:59:27 +0300

    refactor sending tokens to its own function

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,16 @@ const signToken = id => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -20,15 +30,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role
     // passwordChangedAt: req.body.passwordChangedAt
   });
-
-  const token = signToken(newUser._id);
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser
-    }
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -45,11 +47,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
   // if everything is okay, send the token to the client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -155,28 +153,20 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   await user.save();
   // log the user in
 
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   // get user from database
   const user = await User.findById(req.user.id).select('+password');
   //check if the Posted password is correct
-  if (!(await user.correctPassword(req.body.passwordConfirm, user.password))) {
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
     return next(new AppError('incorrect password', 401));
   }
   // if so update the password
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
-  await User.save();
+  await user.save();
   // log the user in
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res);
 });
