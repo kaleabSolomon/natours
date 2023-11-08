197335106e9ecb8e9be244cbc16cd68c04ba33dd
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-08 13:43:40 +0300

    update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   //executing the query
 
@@ -16,7 +25,7 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   // create an error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -26,12 +35,21 @@ exports.updateMe = (req, res, next) => {
       )
     );
   }
+  // filter out valid entries
+  const filteredBody = filterObj(req.body, 'name', 'email');
   // update the user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
 
   res.status(200).json({
-    status: 'success'
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
