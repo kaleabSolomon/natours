42aedea06b0327e91d875f2882c71b54b7c3b073
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-08 13:00:37 +0300

    define user update route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   //executing the query
@@ -14,6 +15,23 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.updateMe = (req, res, next) => {
+  // create an error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'You can not change your password in this route. Please user /updateMyPassword',
+        400
+      )
+    );
+  }
+  // update the user document
+
+  res.status(200).json({
+    status: 'success'
+  });
+};
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
