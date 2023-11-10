7913185adb9d2ecdc5c3ceba5e2a220baf879afc
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-10 15:46:15 +0300

    protect against parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,6 +33,21 @@ app.use(express.json({ limit: '10kb' }));
 
 // DAta Sanitization against noSQL query injections
 app.use(mongoSanitize());
+// data sanitization against xss
+app.use(xss());
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'averageRating',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
